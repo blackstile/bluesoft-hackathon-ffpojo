d92d2d2d46d6240cce6e83298c771e2e031e8459
William <william.miranda@bluesoft.com.br>
2015-07-17 16:27:02 -0300

    criacao dos decorators

--- src/main/java/br/com/bluesoft/Run.java
+++ src/main/java/br/com/bluesoft/Run.java
@@ -1,8 +1,12 @@
 package br.com.bluesoft;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import br.com.bluesoft.beans.RegistroItemVendido;
+import br.com.bluesoft.config.MovOutraConfiguration;
 import br.com.bluesoft.util.FFPojoParserConfiguration;
 
 import com.github.ffpojo.exception.FFPojoException;
@@ -12,7 +16,7 @@ public class Run {
 
     public static void main(String[] args) throws IOException, FFPojoException {
         BufferedReader textFileReader = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/william/Documents/workspace/springbatch/zanthus-parser/src/main/resources/movoutra.txt")));
-        String line =  "951210000020121000002000115061500000007017270000000000000000000000000548000000000000000000000000000000000000000000000009000000000042591100035150603205493000194590000006880000010017791000000001000000688";
+        String line =  null;
         while ( (line = textFileReader.readLine()) != null) {
             Class<?> clazz =  new MovOutraConfiguration().getClassMapped(0,2,line);
             if (clazz==null) continue;

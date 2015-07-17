c793f36e4514b2ed5582a89e3873cca01c77f356
Eric Guilherme <ericguilherme6@gmail.com>
2015-07-17 16:45:45 -0300

    finalizacao do main para leitura do arquivo

--- src/main/java/br/com/bluesoft/Run.java
+++ src/main/java/br/com/bluesoft/Run.java
@@ -1,11 +1,11 @@
 package br.com.bluesoft;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import br.com.bluesoft.beans.RegistroItemVendido;
 import br.com.bluesoft.config.MovOutraConfiguration;
 import br.com.bluesoft.util.FFPojoParserConfiguration;
 
@@ -15,7 +15,10 @@ import com.github.ffpojo.exception.FFPojoException;
 public class Run {
 
     public static void main(String[] args) throws IOException, FFPojoException {
-        BufferedReader textFileReader = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/william/Documents/workspace/springbatch/zanthus-parser/src/main/resources/movoutra.txt")));
+    	ClassLoader classLoader = Run.class.getClassLoader();
+    	File file = new File(classLoader.getResource("movoutra.txt").getFile());
+    	
+        BufferedReader textFileReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
         String line =  null;
         while ( (line = textFileReader.readLine()) != null) {
             Class<?> clazz =  new MovOutraConfiguration().getClassMapped(0,2,line);
@@ -24,6 +27,7 @@ public class Run {
             System.out.println(o);
             System.out.println(line);
             System.out.println(FFPojoParserConfiguration.toText(o));
+            System.out.println();
 
         }
         textFileReader.close();
